91b64fa9ae1ba62a8f68fb31e86f1c8d0fa6c05b
phoro3 <puhro20@gmail.com>
2020-02-11 16:24:59 +0900

    Implement invokevirtual

--- method_invoker.py
+++ method_invoker.py
@@ -1,4 +1,5 @@
 from util import convert_bytes_to_string
+import importlib
 
 class MethodInvoker:
     def __init__(self, constant_pool, method_info):
@@ -19,6 +20,11 @@ class MethodInvoker:
         self.program_counter += 1
         return data
 
+    def get_class_name(self, class_index):
+        return convert_bytes_to_string(
+            self.constant_pool[self.constant_pool[class_index]['name_index']]['bytes']
+            )
+
     def get_name_and_type(self, name_and_type_index):
         name_and_type_info = self.constant_pool[name_and_type_index]
         name_index = name_and_type_info['name_index']
@@ -27,7 +33,6 @@ class MethodInvoker:
             'name': convert_bytes_to_string(self.constant_pool[name_index]['bytes']),
             'type': convert_bytes_to_string(self.constant_pool[descriptor_index]['bytes'])
         }
-
 
     def invoke(self):
         while self.program_counter < self.code_length:
@@ -38,14 +43,17 @@ class MethodInvoker:
         index_byte1 = int.from_bytes(self.read_codes(), 'big')
         index_byte2 = int.from_bytes(self.read_codes(), 'big')
         index = index_byte1 << 8 | index_byte2
+
         field_ref = self.constant_pool[index]
-        class_index =field_ref['class_index']
-        class_name = convert_bytes_to_string(
-            self.constant_pool[self.constant_pool[class_index]['name_index']]['bytes']
-            )
+        class_name = self.get_class_name(field_ref['class_index'])
         name_and_type_index = field_ref['name_and_type_index']
         name_and_type = self.get_name_and_type(name_and_type_index)
-        self.stack.append((class_name + '.' + name_and_type['name'], name_and_type['type']))
+
+        # convert to python module path
+        class_name = class_name.replace('/', '.')
+        target_class = importlib.import_module(class_name)
+        target_field = getattr(target_class, name_and_type['name'])
+        self.stack.append(target_field)
 
     def _ldc(self):
         index = int.from_bytes(self.read_codes(), 'big')
@@ -58,9 +66,23 @@ class MethodInvoker:
     def _invokevirtual(self):
         index_byte1 = int.from_bytes(self.read_codes(), 'big')
         index_byte2 = int.from_bytes(self.read_codes(), 'big')
-        print('invokevirtual')
+        index = index_byte1 << 8 | index_byte2
+
+        # get method name from constant_pool
+        method_ref = self.constant_pool[index]
+        name_and_type = self.get_name_and_type(method_ref['name_and_type_index'])
+        method_name = name_and_type['name']
+
+        # get args from stack
+        arg_types = name_and_type['type']
+        arg_num = len(arg_types.split(';')) - 1
+        args = [self.stack.pop() for _ in range(arg_num)]
+        args.reverse()
+
+        # call method
+        target_method = getattr(self.stack.pop(), method_name)
+        target_method(*args)
 
     def _return(self):
-        print('return')
-
+        pass
 
